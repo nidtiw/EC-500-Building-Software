f02200e8c8338cfdacd1a804a93558c2bd2db7fd
Nidhi T <nidhit@bu.edu>
2018-02-05 17:04:19 -0500

    'removefile'

--- API Assignment/vision_API.py
+++ API Assignment/vision_API.py
@@ -20,9 +20,23 @@ with io.open(file_name, 'rb') as image_file:
 image = types.Image(content=content)
 
 # Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+# response = client.label_detection(image=image)
+# labels = response.label_annotations
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+response = client.landmark_detection(image=image)
+landmarks = response.landmark_annotations
+
+# print('Labels:')
+# for label in labels:
+#     print(label.description)
+
+print('Landmarks:')
+
+for landmark in landmarks:
+	print(landmark.description)
+	for location in landmark.locations:
+		lat_lng = location.lat_lng
+		print('Latitude'.format(lat_lng.latitude))
+		print('Longitude'.format(lat_lng.longitude))
+
+		
